b6a4fecf81a16f19df1e496c33a3e63fca0baca8
Diribar <76135160+Diribar@users.noreply.github.com>
2021-07-29 16:19:02 -0300

    Reescritura de Quiénes Somos

--- rutas y controladores/home_ruta.js
+++ rutas y controladores/home_ruta.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const controlador = require("./home_controlador");
 
 // Controladores *******************************************
+router.get("/login", controlador.loginForm)
 router.get("/", controlador.home);
 
 // Exportarlo **********************************************
