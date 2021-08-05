3820e846c57511b95d67051d0c158c7a317eee7e
Diribar <76135160+Diribar@users.noreply.github.com>
2021-08-05 00:58:09 -0300

    Validaciones on-line terminadas

--- public/javascripts/6-contactanos.js
+++ public/javascripts/6-contactanos.js
@@ -9,12 +9,19 @@ window.addEventListener("load", () => {
 	// Mail
 	let validarMail = document.querySelector("#contactanos form .input[name='mail']");
 	let avisoErrorMail = document.querySelector("#contactanos form .fa-times-circle#mail");
+	// Teléfono
+	let validarTelefono = document.querySelector("#contactanos form .input[name='telefono']");
+	let avisoErrorTelefono = document.querySelector("#contactanos form .fa-times-circle#telefono");
+	// Comentario
+	let validarComentario = document.querySelector("#contactanos form .input[name='comentario']");
+	let avisoErrorComentario = document.querySelector("#contactanos form .fa-times-circle#comentario");
 	// Validar la suma
 	let suma1 = parseInt(document.querySelector("#contactanos input[name='suma1']").value);
 	let suma2 = parseInt(document.querySelector("#contactanos input[name='suma2']").value);
-	let validarSuma = document.querySelector("#contactanos .input[name='validar']");
+	let validarSuma = document.querySelector("#contactanos form .input[name='suma']");
+	let avisoErrorSuma = document.querySelector("#contactanos form .fa-times-circle#suma");
 
-	// Ejecutar las rutinas
+	// Prevenir el submit si hay errores
 	form.addEventListener("submit", (e) => {
 		//e.preventDefault();
 		for (let i=0; i<inputs.length; i++) {
@@ -25,31 +32,54 @@ window.addEventListener("load", () => {
 		}
 	})
 
+	// Validar nombre
 	validarNombre.addEventListener("keypress", (e) => {
-		!/[a-zA-z ]/.test(e.key)
+		!/[A-Z ]/i.test(e.key)
 			? e.preventDefault()
 			: avisoErrorNombre.classList.add("ocultar");
 	});
-
 	validarNombre.addEventListener("change", () => {
 		nombre = validarNombre.value;
-		!/[a-zA-z ]$/.test(nombre)
+		!/^[A-Z ]+$/i.test(nombre)
 			? avisoErrorNombre.classList.remove("ocultar")
 			: avisoErrorNombre.classList.add("ocultar");
 	});
 
-
+	// Validar mail
 	validarMail.addEventListener("change", () => {
 		mail = validarMail.value;
-		!/^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,5}$/.test(mail)
+		!/^[\w\-\.\+]+\@[a-z0-9\.\-]+\.[a-z0-9]{2,5}$/i.test(mail)
 			? avisoErrorMail.classList.remove("ocultar")
 			: avisoErrorMail.classList.add("ocultar");
 	});
 
-	validarSuma.addEventListener("change", () => {
-		(suma1 + suma2 != validarSuma.value)
-			? avisoError[avisoError.length - 1].classList.remove("ocultar")
-			: avisoError[avisoError.length - 1].classList.add("ocultar");
+	// Validar teléfono
+	validarTelefono.addEventListener("keypress", (e) => {
+		!/[0-9 -()/+]/.test(e.key)
+			? e.preventDefault()
+			: avisoErrorTelefono.classList.add("ocultar");
+	});
+	validarTelefono.addEventListener("change", () => {
+		telefono = validarTelefono.value;
+		!/^[0-9 -()/+]+$/.test(telefono)
+			? avisoErrorTelefono.classList.remove("ocultar")
+			: avisoErrorTelefono.classList.add("ocultar");
 	});
 
+	// Validar comentario
+	validarComentario.addEventListener("keypress", () => {
+		avisoErrorComentario.classList.add("ocultar");
+	});
+
+	// Validar suma
+	validarSuma.addEventListener("keypress", (e) => {
+		!/[0-9]/.test(e.key)
+			? e.preventDefault()
+			: avisoErrorTelefono.classList.add("ocultar");
+	});
+	validarSuma.addEventListener("change", () => {
+		suma1 + suma2 != validarSuma.value
+			? avisoErrorSuma.classList.remove("ocultar")
+			: avisoErrorSuma.classList.add("ocultar");
+	});
 })
