2c7e0bbcdbf30074d3d0deb6de49f4d8690590a7
Diribar <76135160+Diribar@users.noreply.github.com>
2021-08-30 18:40:24 -0300

    Apertura del controlador en dos

--- rutas y controladores/controlador_home.js
+++ rutas y controladores/controlador_home.js
@@ -46,20 +46,6 @@ module.exports = {
 		res.redirect("/editar/home");
 	},
 
-	editarHomeForm: async (req, res) => {
-		let encabezado = await BD_varios.ObtenerColoresEncabezado();
-		//return res.send(encabezado[1]);
-		res.render("1-editarHome", {
-			encabezado: encabezado[0],
-			titulos: await BD_varios.ObtenerTitulos(),
-			footer: encabezado[1],
-			colores: await BD_varios.ObtenerColores(),
-		});
-	},
-
-	editarTexto: (req, res) => {
-		res.send("Editar texto")
-	}
 };
 
 // Función enviar mail
--- rutas y controladores/ruta.js
+++ rutas y controladores/ruta.js
@@ -1,20 +1,25 @@
 // Requires ************************************************
 var express = require("express");
 var router = express.Router();
-const controlador = require("./controlador");
+const controlador_home = require("./controlador_home");
+const controlador_editar = require("./controlador_editar");
 
 // Controladores *******************************************
-router.get("/", controlador.home);
-router.get("/form", controlador.contactanosFrontEnd);
-router.post("/form", controlador.contactanosBackEnd);
+router.get("/", controlador_home.home);
+router.get("/contactanos", controlador_home.contactanosFrontEnd);
+router.post("/contactanos", controlador_home.contactanosBackEnd);
 
-router.get("/login", controlador.loginForm)
+router.get("/login", controlador_home.loginForm)
 
-router.get("/editar/home", controlador.editarHomeForm);
-router.get("/editar/colores", controlador.editarHomeForm);
-router.get("/editar/texto/:id", controlador.editarHomeForm);
-router.get("/editar/imagenes/inicio", controlador.editarHomeForm);
-router.get("/editar/imagenes/proyectos", controlador.editarHomeForm);
+router.get("/editar/home", controlador_editar.editarHomeForm);
+router.get("/editar/colores", controlador_editar.editarHomeForm);
+router.get("/editar/texto/:id", controlador_editar.editarHomeForm);
+router.get("/editar/imagenes/inicio", controlador_editar.editarHomeForm);
+router.get("/editar/imagenes/proyectos", controlador_editar.editarHomeForm);
+
+// APIs ****************************************************
+//router.get("/editar/orden_obtener_id", controlador_editar.orden_obtener_id);
+//router.get("/editar/id_fijar_orden", controlador_editar.id_fijar_orden);
 
 // Exportarlo **********************************************
 module.exports = router;
