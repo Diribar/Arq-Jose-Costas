5f4d2c14837266a99edc6560741c1dcb82ce7f2a
Diribar <76135160+Diribar@users.noreply.github.com>
2021-08-30 18:56:44 -0300

    Miscelaneas

---
+++ rutas y controladores/controlador_APIs.js
@@ -0,0 +1,12 @@
+// **** Requires ***********
+const BD_varios = require("../base_de_datos/config/BD_varios");
+
+// **** Exportar ***********
+module.exports = {
+	editarTexto: (req, res) => {
+		res.send("Editar texto")
+	},
+	home: async (req, res) => {
+
+	},
+};
