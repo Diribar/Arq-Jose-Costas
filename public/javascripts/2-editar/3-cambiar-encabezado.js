3816cb2c4089325005408405daa7856d5544b3ef
Diribar <76135160+Diribar@users.noreply.github.com>
2021-08-31 08:05:17 -0300

    Cambio de colores en encabezado y footer

--- public/javascripts/2-editar/3-cambiar-encabezado.js
+++ public/javascripts/2-editar/3-cambiar-encabezado.js
@@ -13,12 +13,37 @@ window.addEventListener("load", () => {
 		"select[name='color_letras_footer']"
 	);
 
+	// Cambiar los colores del encabezado
+	let cfe = document.querySelector("#cfe").innerHTML;
+	let cle = document.querySelector("#cle").innerHTML;
+	document.querySelector("#encabezado").style.backgroundColor = cfe;
+	opciones = document.querySelectorAll("#encabezado option");
+	for (opcion of opciones) {
+		opcion.style.backgroundColor = cfe
+	}
+	document.querySelector("#encabezado").style.color = cle;
+	document.querySelector("#encabezado strong").style.color = cle;
+	document.querySelectorAll("#encabezado select")[0].style.color = cle;
+	document.querySelectorAll("#encabezado select")[1].style.color = cle;
+
+	// Cambiar los colores del footer
+	let cff = document.querySelector("#cff").innerHTML;
+	let clf = document.querySelector("#clf").innerHTML;
+	document.querySelector("#footer").style.backgroundColor = cff;
+	opciones = document.querySelectorAll("#footer option");
+	for (opcion of opciones) {
+		opcion.style.backgroundColor = cff;
+	}
+	document.querySelector("#footer").style.color = clf;
+	document.querySelector("#footer strong").style.color = clf;
+	document.querySelectorAll("#footer select")[0].style.color = clf;
+	document.querySelectorAll("#footer select")[1].style.color = clf;
+
 	// Acciones si se cambia un valor
 	color_fondo_encabezado.addEventListener("change", () => {
 		dato = color_fondo_encabezado.value;
 		campo = "color_fondo_id";
 		cambiarColor(1, dato, campo);
-		
 	});
 	color_letras_encabezado.addEventListener("change", () => {
 		dato = color_letras_encabezado.value;
@@ -37,8 +62,8 @@ window.addEventListener("load", () => {
 	});
 });
 
-const cambiarColor = (id, dato, campo) => {
-	fetch(
+const cambiarColor = async (id, dato, campo) => {
+	await fetch(
 		"/editar/cambiar_encabezado/?id=" +
 			id +
 			"&dato=" +
@@ -46,4 +71,5 @@ const cambiarColor = (id, dato, campo) => {
 			"&campo=" +
 			campo
 	);
+	location.reload();
 };
