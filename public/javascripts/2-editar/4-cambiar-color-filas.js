f48f386a2ac5a89cc6c1cf96dd9c82a98b560b75
Diribar <76135160+Diribar@users.noreply.github.com>
2021-08-31 10:57:19 -0300

    Colores de filas

--- public/javascripts/2-editar/4-cambiar-color-filas.js
+++ public/javascripts/2-editar/4-cambiar-color-filas.js
@@ -1,45 +1,37 @@
 window.addEventListener("load", () => {
 	// Variables generales
-	let color_fondo_encabezado = document.querySelector(
-		"select[name='color_fondo_encabezado']"
+	let color_fondo_fila = document.querySelector(
+		"select[name='color_fondo_filas']"
 	);
-	let color_letras_encabezado = document.querySelector(
-		"select[name='color_letras_encabezado']"
+	let color_letras_fila = document.querySelector(
+		"select[name='color_letras_filas']"
 	);
 
 	// Cambiar los colores
-	let cfe = document.querySelector("#cfe").innerHTML;
-	let cle = document.querySelector("#cle").innerHTML;
-	document.querySelector("#encabezado").style.backgroundColor = cfe;
-	opciones = document.querySelectorAll("#encabezado option");
-	for (opcion of opciones) {
-		opcion.style.backgroundColor = cfe
+	filas = document.querySelectorAll(".fila");
+	for (let i = 0; i < filas.length; i++) {
+		cff = document.querySelector(".fila.numero" + i + " .cff").innerHTML;
+		clf = document.querySelector(".fila.numero" + i + " .clf").innerHTML;
+		document.querySelector(".fila.numero" + i).style.backgroundColor = cff;
+		document.querySelector(".fila.numero" + i).style.color = clf;
+		document.querySelector(".fila.numero" + i + " strong").style.color =
+			clf;
+		document.querySelectorAll(
+			".fila.numero" + i + " input"
+		)[0].style.color = clf;
+		document.querySelectorAll(
+			".fila.numero" + i + " input"
+		)[1].style.color = clf;
+		document.querySelectorAll(
+			".fila.numero" + i + " select"
+		)[0].style.color = clf;
+		document.querySelectorAll(
+			".fila.numero" + i + " select"
+		)[1].style.color = clf;
+		opciones = document.querySelectorAll(".fila.numero" + i + " option");
+		for (opcion of opciones) {
+			opcion.style.backgroundColor = cff;
+		}
 	}
-	document.querySelector("#encabezado").style.color = cle;
-	document.querySelector("#encabezado strong").style.color = cle;
-	document.querySelectorAll("#encabezado select")[0].style.color = cle;
-	document.querySelectorAll("#encabezado select")[1].style.color = cle;
 
-	// Acciones si se cambia un valor
-	color_fondo_encabezado.addEventListener("change", () => {
-		dato = color_fondo_encabezado.value;
-		campo = "color_fondo_id";
-		changeColor(1, dato, campo);
-	});
-	color_letras_encabezado.addEventListener("change", () => {
-		dato = color_letras_encabezado.value;
-		campo = "color_letras_id";
-		changeColor(1, dato, campo);
-	});
 });
-
-const changeColor = async (id, dato, campo) => {
-	fetch(
-		"/editar/cambiar_filas/?orden=" +
-			orden +
-			"&dato=" +
-			dato +
-			"&campo=" +
-			campo
-	);
-};
