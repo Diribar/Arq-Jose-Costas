9e5f061156351f6d239c458a582621d6b8160a8d
Diribar <76135160+Diribar@users.noreply.github.com>
2021-09-02 12:37:23 -0300

    Miscelaneas

--- base_de_datos/modelos/5-quienes_somos_clientes.js
+++ base_de_datos/modelos/5-quienes_somos_clientes.js
@@ -2,9 +2,10 @@ module.exports = (sequelize, dt) => {
 	const alias = "clientes_imagenes";
 	const columns = {
 		id: { type: dt.INTEGER, primaryKey: true },
+		grupo: { type: dt.INTEGER },
 		orden: { type: dt.INTEGER },
 		nombre: { type: dt.STRING(50) },
-		imagen: { type: dt.STRING(50) },
+		archivo: { type: dt.STRING(50) },
 	};
 	const config = {
 		tableName: "5_quienes_somos_clientes",
