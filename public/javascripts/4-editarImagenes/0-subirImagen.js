0a479f7540c8cf7e00cb2fe416d31b2a0b0f1f3f
Diribar <76135160+Diribar@users.noreply.github.com>
2021-09-10 19:19:37 -0300

    Logré poner la imagen original como input

--- public/javascripts/4-editarImagenes/0-subirImagen.js
+++ public/javascripts/4-editarImagenes/0-subirImagen.js
@@ -1,24 +1,22 @@
 window.addEventListener("load", () => {
-	let src = document.querySelectorAll("#src");
-	
-	document.getElementById("inputImagen").onchange = (e) => {
-		// Ocultar y mostrar elementos
-		document.getElementById("labelImagen").classList.add("ocultar");
-		mostrarImagen(e);
-	};
+	let inputImagen = document.querySelectorAll("#inputImagen");
+	console.log(inputImagen[1]);
+	for (let i = 1; i < inputImagen.length; i++) {
+		inputImagen[i].addEventListener("change", (e) => {
+			console.log(i);
+			// Creamos el objeto de la clase FileReader
+			let reader = new FileReader();
+			// Leemos el archivo subido y se lo pasamos a nuestro fileReader
+			reader.readAsDataURL(e.target.files[0]);
+			// Le decimos que cuando esté listo ejecute el código interno
+			reader.onload = () => {
+				preview = document.querySelectorAll("#preview");
+				console.log[preview];
+				image = document.createElement("img");
+				image.src = reader.result;
+				preview[i].innerHTML = "";
+				preview[i].append(image);
+			};
+		});
+	}
 });
-
-let mostrarImagen = (e) => {
-	// Creamos el objeto de la clase FileReader
-	let reader = new FileReader();
-	// Leemos el archivo subido y se lo pasamos a nuestro fileReader
-	reader.readAsDataURL(e.target.files[0]);
-	// Le decimos que cuando esté listo ejecute el código interno
-	reader.onload = () => {
-		let preview = document.getElementById("preview");
-		image = document.createElement("img");
-		image.src = reader.result;
-		preview.innerHTML = "";
-		preview.append(image);
-	};
-};
