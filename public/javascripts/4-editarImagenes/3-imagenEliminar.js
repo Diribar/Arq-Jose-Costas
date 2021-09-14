3fb36fc6956fc01c8d7b74973390ed4ae4c926de
Diribar <76135160+Diribar@users.noreply.github.com>
2021-09-14 11:31:57 -0300

    Miscelaneas

--- public/javascripts/4-editarImagenes/3-imagenEliminar.js
+++ public/javascripts/4-editarImagenes/3-imagenEliminar.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", () => {
 	// Variables generales
-	let entidad = document.querySelector("input[name='entidad']").value;
 	let id = document.querySelectorAll(".imagenes i.ocultar");
 	let eliminar = document.querySelectorAll(".imagenes i.fa-trash-alt");
 
@@ -8,13 +7,14 @@ window.addEventListener("load", () => {
 	for (let i = 0; i < id.length; i++) {
 		eliminar[i].addEventListener("click", async () => {
 			dato_id = id[i].innerHTML;
-			await eliminarImagen(entidad, dato_id);
+			await eliminarImagen(dato_id);
 		});
 	}
 });
 
-const eliminarImagen = async (entidad, id) => {
-	ruta = document.querySelector("input[name='ruta']").value;
+const eliminarImagen = async (id) => {
+	let entidad = document.querySelector("input[name='entidad']").value;
+	let ruta = document.querySelector("input[name='ruta']").value;
 	ruta = encodeURIComponent(ruta);
 	await fetch("/editar/eliminarregistro/?entidad=" + entidad + "&id=" + id + "&ruta=" + ruta);
 	location.reload();
