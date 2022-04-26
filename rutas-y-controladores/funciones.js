3dc756bf4ea61737c1a93f20aab33a640a85e03c
unknown <diegoiribarren2015@gmail.com>
2022-04-26 12:38:44 -0300

    Miscelaneas

--- rutas-y-controladores/funciones.js
+++ rutas-y-controladores/funciones.js
@@ -5,26 +5,18 @@ const fs = require("fs");
 
 module.exports = {
 	enviarMail: async (asunto, nombre, mail, telefono, comentario) => {
-		// Remitente
-		let remitente = "mensaje.web.02@gmail.com"; // dirección de gmail
-		let contrasena = process.env.contrMail; // contraseña de gmail
 		// create reusable transporter object using the default SMTP transport
 		let transporter = nodemailer.createTransport({
-			//service: "gmail",
 			host: "smtp.gmail.com",
 			port: 465,
 			secure: true, // true for 465, false for other ports
 			auth: {
-				user: remitente,
+				user: process.env.direccMail, // dirección de gmail
 				pass: process.env.contrAplicacion, // contraseña de aplicación de gmail
 			},
 		});
-		// transporter.verify().then(() => {
-		//console.log("Listo para enviar mails");
-		// });
-		// send mail with defined transport object
-		datos = {
-			from: '"arquitectojosecostas.com.ar" <' + remitente + ">",
+		let datos = {
+			from: '"arquitectojosecostas.com.ar" <' + process.env.direccMail + ">",
 			to: "josericardocostas@hotmail.com",
 			subject: asunto,
 			html:
@@ -40,13 +32,6 @@ module.exports = {
 		//await transporter.sendMail(datos);
 		datos.to = "diegoiribarren2015@gmail.com";
 		await transporter.sendMail(datos);
-		// await transporter.sendMail(info, (error, info) => {
-		// 	if (error) {
-		// 		console.log(error);
-		// 	} else {
-		// 		console.log("Email sent: " + info.response);
-		// 	}
-		// });
 	},
 
 	eliminarImagen: (ruta, nombre) => {
