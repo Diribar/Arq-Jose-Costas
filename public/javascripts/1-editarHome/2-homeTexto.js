d08449e808ee32ee32324cd18fa5ad715e835b1e
diribar <diegoiribarren2015@gmail.com>
2023-10-07 00:38:45 -0300

    Mejora de procesos

--- public/javascripts/1-editarHome/2-homeTexto.js
+++ public/javascripts/1-editarHome/2-homeTexto.js
@@ -1,24 +1,21 @@
 window.addEventListener("load", async () => {
 	// Acciones si se cambia un valor
-	let IDs = document.querySelectorAll(".filas #id");
-	let ne = document.querySelectorAll(".filas input[name='ne']"); // Nombre en el encabezado
-	let ts = document.querySelectorAll(".filas input[name='ts']"); // Título en la sección
-	let scff = document.querySelectorAll(".filas select[name='cff']"); // Color de fondo
-	let sclf = document.querySelectorAll(".filas select[name='clf']"); // Color de letra
+	const IDs = document.querySelectorAll(".filas #id");
+	const ne = document.querySelectorAll(".filas input[name='ne']"); // Nombre en el encabezado
+	const ts = document.querySelectorAll(".filas input[name='ts']"); // Título en la sección
+	const scff = document.querySelectorAll(".filas select[name='cff']"); // Color de fondo
+	const sclf = document.querySelectorAll(".filas select[name='clf']"); // Color de letra
+
 	for (let i = 0; i < ts.length; i++) {
-		// Validar sintaxis y largo
-		ne[i].addEventListener("input", () => {
-			OKne = validar(ne[i], 20);
-		});
-		ts[i].addEventListener("input", () => {
-			OKts = validar(ts[i], 50);
-		});
-		// Hacer cambios en la BD
+		// valida sintaxis y largo
+		ne[i].addEventListener("keypress", (e) => valida(e, 20));
+		ts[i].addEventListener("keypress", () => valida(ts[i], 50));
+		// Hace cambios en la BD
 		ne[i].addEventListener("change", async () => {
-			OKne ? await funcionTexto(ne[i], IDs[i], "nombre_encabezado") : "";
+			if (validaCaracteres(ne[i])) await funcionTexto(ne[i], IDs[i], "nombre_encabezado");
 		});
 		ts[i].addEventListener("change", async () => {
-			OKts ? await funcionTexto(ts[i], IDs[i], "titulo_seccion") : "";
+			if (validaCaracteres(ts[i])) await funcionTexto(ts[i], IDs[i], "titulo_seccion");
 		});
 		scff[i].addEventListener("change", async () => {
 			await funcionTexto(scff[i], IDs[i], "color_fondo_id");
@@ -30,33 +27,35 @@ window.addEventListener("load", async () => {
 });
 
 // FÓRMULAS *************************************************
-let validar = (campo, largoMax) => {
-	campo.addEventListener("keypress", (e) => {
-		campo.value.length >= largoMax ? e.preventDefault() : "";
-	});
-	valNombre = /^[A-Z][A-Za-z áéíóúüñ\d+-]+$/;
-	valNombre.test(campo.value) && campo.value.length <= largoMax
-		? (contenidoOK = true)
-		: (contenidoOK = false);
-	!contenidoOK
-		? campo.classList.add("rojo")
-		: campo.classList.remove("rojo");
-	return contenidoOK
+let valida = (e, largoMax) => {
+	// Si se supera el largo, previene el 'keypress'
+	if (e.target.value.length >= largoMax) return e.preventDefault();
+
+	// Cambia el color del texto si el contenido no está aprobado
+	const contenidoOK = validaCaracteres(e.target);
+	contenidoOK ? campo.classList.remove("rojo") : campo.classList.add("rojo");
+
+	// Fin
+	return;
+};
+let validaCaracteres = (campo) => {
+	// Variables
+	const campo = e.target;
+	const valNombre = /^[A-Z][A-Za-z áéíóúüñ\d+-]+$/;
+	const contenidoOK = valNombre.test(campo.value);
+
+	//Fin
+	return contenidoOK;
 };
 
 let funcionTexto = async (celda, ID, campo) => {
-		id = ID.innerHTML;
-		dato = celda.value;
-		await fetch(
-			"/editar/cambiarvalor/?entidad=titulos" +
-				"&id=" +
-				id +
-				"&dato=" +
-				dato +
-				"&campo=" +
-				campo
-		);
-		campo == "color_fondo_id" || campo == "color_letras_id"
-			? location.reload()
-			: "";
+	// Variables
+	const id = ID.innerHTML;
+	const dato = celda.value;
+
+	// Cambia el valor
+	await fetch("/edicion/cambia-el-valor/?entidad=titulos" + "&id=" + id + "&dato=" + dato + "&campo=" + campo);
+	// Fin
+	if (campo == "color_fondo_id" || campo == "color_letras_id") return location.reload();
+	else return;
 };
