7ad7528bf5e2eb938c799af9e9b6245eb9b58c76
diribar <diegoiribarren2015@gmail.com>
2023-10-07 00:06:40 -0300

    Mejora de procesos

--- public/javascripts/0-home/login.js
+++ public/javascripts/0-home/login.js
@@ -1,20 +1,15 @@
 window.addEventListener("load", () => {
-	let anchors = document.querySelectorAll("#arquitecto a");
-	let inputs = document.querySelectorAll("#contactanos form .input");
-	let suma1 = document.querySelector("#contactanos #suma1");
-	let suma2 = document.querySelector("#contactanos #suma2");
-	let suma = document.querySelector("#contactanos #suma");
+	const anchors = document.querySelectorAll("#arquitecto a");
+	const inputs = document.querySelectorAll("#contactanos form .input");
+	const suma1 = document.querySelector("#contactanos #suma1");
+	const suma2 = document.querySelector("#contactanos #suma2");
+	const suma = document.querySelector("#contactanos #suma");
 
 	for (anchor of anchors) {
 		anchor.addEventListener("click", (e) => {
 			let limpio = true;
-			for (let i = 0; i < inputs.length - 1; i++) {
-				inputs[i].value != "" ? (limpio = false) : "";
-			}
-			parseInt(suma1.innerHTML) + parseInt(suma2.innerHTML) ==
-				suma.value && limpio
-				? (window.location.href = "/loginnuevo")
-				: "";
+			for (let i = 0; i < inputs.length - 1; i++) if (inputs[i].value) limpio = false;
+			if (parseInt(suma1.innerHTML) + parseInt(suma2.innerHTML) == suma.value && limpio) window.location.href = "/login";
 		});
 	}
 });
