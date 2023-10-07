72f7c789275aa2541c3aa9ec7ed178f0fac36bac
diribar <diegoiribarren2015@gmail.com>
2023-10-07 00:48:08 -0300

    Mejora continua

--- middlewares/soloUsuarios.js
+++ middlewares/soloUsuarios.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
 	// Redirecciona
-	if (!req.cookies.aceptado) return res.redirect("/");
+	if (!req.cookies.aceptado) return res.redirect("/login");
 
 	// Fin
 	next();
