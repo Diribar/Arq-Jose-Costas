f2ddb05b000ce93120532243797f7a79c588e43d
diribar <diegoiribarren2015@gmail.com>
2023-10-10 19:34:20 -0300

    Miscelaneas

--- middlewares/soloUsuarios.js
+++ middlewares/soloUsuarios.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
 	// Redirecciona
-	//if (!req.cookies.aceptado) return res.redirect("/login");
+	if (!req.cookies.aceptado) return res.redirect("/login");
 
 	// Fin
 	next();
