428657a39e65b0a5beffc247cd910abef7dbbd05
diribar <diegoiribarren2015@gmail.com>
2024-04-04 04:06:38 -0300

    Mejora de procesos

--- base_de_datos/config/config.js
+++ base_de_datos/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	development: {
 		username: "root",
 		password: "",
-		database: "jose_costas",
+		database: process.env.DB_NAME,
 		host: "127.0.0.1",
 		dialect: "mysql",
 		logging: false,
