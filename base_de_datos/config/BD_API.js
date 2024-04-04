06532644f4bfd8996c8d5975f025ffee7f0d3f31
diribar <diegoiribarren2015@gmail.com>
2024-04-04 04:10:32 -0300

    Mejora de procesos

--- base_de_datos/config/BD_API.js
+++ base_de_datos/config/BD_API.js
@@ -2,16 +2,16 @@ const db = require("../modelos");
 
 module.exports = {
 	OrdenarRegistros: (entidad, id, orden) => {
-		return db[entidad].update({ orden: orden }, { where: { id: id } });
+		return db[entidad].update({orden: orden}, {where: {id}});
 	},
 
 	CambiarValor: (entidad, id, dato, campo) => {
-		return db[entidad].update({ [campo]: dato }, { where: { id: id } });
+		return db[entidad].update({[campo]: dato}, {where: {id}});
 	},
 
 	EliminarRegistro: (entidad, id) => {
 		return db[entidad].destroy({
-			where: { id: id },
+			where: {id},
 		});
 	},
 
@@ -32,7 +32,7 @@ module.exports = {
 
 	EliminarGrupo: (entidad, grupo) => {
 		return db[entidad].destroy({
-			where: { grupo: grupo },
+			where: {grupo: grupo},
 		});
 	},
 };
