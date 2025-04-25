0a0f8adcdeb8393ef765e1c90c11f925d0c97dbe
diribar <diegoiribarren2015@gmail.com>
2025-04-25 08:16:01 -0300

    Miscelaneas

--- app.js
+++ app.js
@@ -46,3 +46,7 @@ global.entDesarr = !entProducc && !entPrueba;
 // Listener
 const puerto = entProducc ? 4200 : entPrueba ? 4206 : 3000;
 app.listen(puerto, () => console.log("\nJosé Costas - Servidor funcionando...")); // Para conectarse con el servidor
+
+// Rutina para dar "señales de vida" al servidor
+const cron = require("node-cron");
+cron.schedule("0 0 * * *", () => console.log(new Date()), {timezone: "America/Buenos_Aires"}); // Rutinas diarias (a las 0:00hs)
