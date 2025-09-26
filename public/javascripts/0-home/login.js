61119a93cf8942d2b1c0bff1b1612fd270f00957
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-09-26 01:16:17 -0300

    Mejora continua

--- public/javascripts/0-home/login.js
+++ public/javascripts/0-home/login.js
@@ -1,20 +1,21 @@
 window.addEventListener("load", () => {
-	const anchors = document.querySelectorAll("#arquitecto a");
-	const inputs = document.querySelectorAll("#contactanos form .input");
+	const imgs = document.querySelectorAll("#arquitecto img");
+	const inputs = Array.from(document.querySelectorAll("#contactanos form .input")).slice(0,-1);
 	const suma1 = document.querySelector("#contactanos #suma1");
 	const suma2 = document.querySelector("#contactanos #suma2");
 	const suma = document.querySelector("#contactanos #suma");
 
-	for (anchor of anchors) {
-		anchor.addEventListener("click", () => {
+	for (img of imgs) {
+		img.addEventListener("click", async () => {
+
 			// Si algún valor del formulario no está vacío, interrumpe la función
 			for (const input of inputs) if (input.value) return;
 
 			// Si la suma es incorrecta, interrumpe la función
 			if (parseInt(suma1.innerHTML) + parseInt(suma2.innerHTML) != suma.value) return;
 
-			// Envía el mail
-
+			// Envía un mail con el código de login
+			await fetch("/api-login");
 
 			// Redirige al login
 			window.location.href = "/login";
