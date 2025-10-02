4589789396caaedf081275d315dee5ea47a03cff
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-02 14:50:33 -0300

    Mejora de procesos

--- rutas-y-controladores/controladorVista.js
+++ rutas-y-controladores/controladorVista.js
@@ -33,10 +33,12 @@ module.exports = {
 		form: async (req, res) => res.render("login", {mailEnviado: req.session.mailEnviado}),
 		guardar: (req, res) => {
 			// Verifica el código
-			if (req.body.codigo == req.session.codigo) res.cookie("aceptado", true, {maxAge: 60 * 60 * 1000});
+			if (req.body.codigo == req.session.codigo) res.cookie("aceptado", true, {maxAge: 60 * 60 * 1000}); // una hora
 
 			// Destino
 			const destino = req.body.codigo == req.session.codigo ? "/edicion/home" : "/login";
+			if (req.body.codigo == req.session.codigo) console.log("Login exitoso");
+			else console.log(39, req.body.codigo, req.session.codigo);
 
 			// Fin
 			return res.redirect(destino);
