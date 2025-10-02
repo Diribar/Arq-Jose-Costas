09774741eca125b5424ed70170428f8ead34143e
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-10-02 14:38:47 -0300

    Mejora de procesos

--- public/javascripts/0-home/dropDowns.js
+++ public/javascripts/0-home/dropDowns.js
@@ -2,23 +2,15 @@ window.addEventListener("load", () => {
 	let homeButton = document.querySelector("#home-button");
 	let proyectosButton = document.querySelector("#proyectos-button");
 
+	// Eventos
 	homeButton.addEventListener("click", () => desplegable("menu-home"));
-	proyectosButton.addEventListener("click", () =>
-		desplegable("menu-proyectos")
-	);
-
-	// Cerrar los dropdowns en desuso
+	proyectosButton.addEventListener("click", () => desplegable("menu-proyectos"));
 	window.onclick = function (e) {
-		!e.target.matches("#home-button")
-			? document.getElementById("menu-home").classList.add("ocultar")
-			: "";
-		!e.target.matches("#proyectos-button")
-			? document.getElementById("menu-proyectos").classList.add("ocultar")
-			: "";
+		// Cierra los dropdowns en desuso
+		!e.target.matches("#home-button") && document.getElementById("menu-home").classList.add("ocultar");
+		!e.target.matches("#proyectos-button") && document.getElementById("menu-proyectos").classList.add("ocultar");
 	};
 });
 
 // Alternar la visibilidad de los Menús Desplegables
-const desplegable = (n) => {
-	document.getElementById(n).classList.toggle("ocultar");
-}
+const desplegable = (n) => document.getElementById(n).classList.toggle("ocultar");
